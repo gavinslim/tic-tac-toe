d039a218d92b412e9c20df0abebccf39c3b21172
gavin <gavin.lim@alumni.ubc.ca>
2022-01-16 01:21:39 -0800

    Player markers display on board

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const squares = document.getElementsByClassName('square');
-const player = document.getElementById('player-toggle');
+const single_player = document.getElementById('player-toggle');
 const comp_container = document.getElementById('ai-select');
 const comp_enable = document.getElementById('ai-toggle');
+const start_button = document.getElementById('start');
 
-// Add click event listener to gameboard
+// Gameboard
 for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', (e) => {
-        console.log(e.currentTarget.getAttribute('id'));
-        gameboard.display()
+        
+        // Which square is written
+        var coordinate = e.currentTarget.getAttribute('id');
+        var row = coordinate.charAt(0);
+        var col = coordinate.charAt(1);
+
+        // console.log(player1.active);
+        if (player1.active == true) {
+            // console.log('true');
+            player1.mark(row, col, 'X');
+            player1.active = false;
+        } else {
+            // console.log('false');
+            player2.mark(row, col, 'O');
+            player1.active = true;
+        }
+        
     });
 }
 
-// Add click event listener for setup
-player.addEventListener('click', (e) => {
-    if (player.checked == true) {
+// Player and Computer Setup
+single_player.addEventListener('click', (e) => {
+    if (single_player.checked == true) {
         comp_container.classList.add('inactive');
         comp_enable.checked = false;
     } else {
@@ -21,8 +37,20 @@ player.addEventListener('click', (e) => {
     }
 });
 
+// Start button 
+start_button.addEventListener('click', () => {
+    if (start_button.innerHTML == 'Start') {
+        start_button.innerHTML = 'Reset';
+        gameboard.activate();
+    } else {
+        start_button.innerHTML = 'Start';
+        gameboard.reset();
+    }    
+})
+
 
 const gameboard = (() => {
+    var _active = false;
 
     // Gameboard 2D array (private)
     var _board = [
@@ -31,6 +59,23 @@ const gameboard = (() => {
         ["","",""]
     ];
 
+    // Reset gameboard 
+    const reset = () => {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                _board[i][j] = '';
+                let element = document.getElementById(`${i}${j}`);
+                while(element.firstChild) element.removeChild(element.firstChild);
+            }
+        } 
+        _active = false;      
+    }
+
+    // Activate gameboard
+    const activate = () => {
+        _active = true;
+    }
+
     // Console log gameboard
     const print = () => {console.log(_board);}
 
@@ -38,28 +83,60 @@ const gameboard = (() => {
     const display = () => {
         for(let i = 0; i < 3; i++) {
             for(let j = 0; j < 3; j++) {
-                document.getElementById(`${i}${j}`).innerHTML = _board[i][j];
+                // let element = document.getElementById(`${i}${j}`);
+                // while(element.firstChild) element.removeChild(element.firstChild);
             }
         }
     }
 
     // Mark on gameboard
-    const mark = (row, col) => {
-        _board[row][col] = 'X';
+    const mark = (row, col, marker) => {
+        if (!_active) {return;}   
+
+        // Modify gameboard array
+        _board[row][col] = marker;
+
+        // Modify front end display
+        var parent = document.getElementById(`${row}${col}`);
+        var child = document.createElement('div');
+
+        if (marker == 'X') {
+            child.classList.add('fas', 'fa-times');
+            parent.appendChild(child);
+        } else if (marker == 'O') {
+            child.classList.add('fas', 'fa-circle');
+            parent.appendChild(child);
+            
+            var child1 = document.createElement('div');
+            child1.classList.add('fas', 'fa-circle', 'smaller');
+            parent.appendChild(child1);
+        }
     }
 
-    return {print, display, mark};
+    return {reset, activate, print, mark};
 
 })();
 
 const Player = (name) => {
+    // <i class="fas fa-times"></i>
 
+    var active = false;
+
+    const mark = (row, col, marker) => {
+        gameboard.mark(row, col, marker);
+    }
+
+    const get_name = () => {
+        console.log(name);
+    }
+
+    return {mark, get_name, active};
 }
 
+const player1 = Player('Gavin');
+player1.active = true;
+const player2 = Player('Gabriel');
 
-// gameboard.print();
-gameboard.display();
-gameboard.mark(0, 0);
 
 //Todo
 /* 
