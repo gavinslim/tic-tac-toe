4bd107dad84741d65dc0d1003357f134dddeef34
gavin <gavin.lim@alumni.ubc.ca>
2022-01-17 01:20:49 -0800

    Add computer play and message display

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
+
+/* 
+Features
+- [Done] Ability to select one player or 2 players (toggle)
+- [Done] Display to show whose turn it is
+- AI for computer
+- Factory and Module functions
+*/
+
+const first_player_name = document.getElementById('p1-name');
+const second_player_name = document.getElementById('p2-name');
+const second_player = document.getElementById('player-toggle');
+const second_player_input = document.getElementsByClassName('player-2')[0]; 
+const message = document.getElementsByClassName('message')[0];
 const squares = document.getElementsByClassName('square');
-const single_player = document.getElementById('player-toggle');
-const comp_container = document.getElementById('ai-select');
-const comp_enable = document.getElementById('ai-toggle');
 const start_button = document.getElementById('start');
 const win_line = document.getElementById('line');
+
+// Player and Computer Setup
+second_player.addEventListener('click', (e) => {
+    if (second_player.checked == true) {
+        second_player_input.classList.add('active');
+    } else {
+        second_player_input.classList.remove('active');
+    }
+
+    gameboard.reset();
+    start_button.innerHTML = 'Start';
+});
+
+// Start button 
+start_button.addEventListener('click', () => {
+
+    // Check input fields are filled
+    if (first_player_name.value == '') {
+        message.innerHTML = 'Missing Player 1 name!';
+        return;
+    }
+    if ((second_player.checked == true) && (second_player_name.value == '')) {
+        message.innerHTML = 'Missing Player 2 name!';
+        return;
+    }
+
+    if (start_button.innerHTML == 'Start') {
+        message.innerHTML = '';
+    } 
+
+    // Initialize 
+    start_button.innerHTML = 'Reset';
+    gameboard.reset();
+    gameboard.activate();
+
+    player1 = Player(first_player_name.value);
+    player1.active = true; 
+
+    if (second_player.checked == true) {
+        player2 = Player(second_player_name.value)
+    } else {
+        player2 = Player('Computer');
+    }
+    
+})
 
 // Gameboard
 for (var i = 0; i < squares.length; i++) {
@@ -14,39 +70,45 @@ for (var i = 0; i < squares.length; i++) {
         var row = coordinate.charAt(0);
         var col = coordinate.charAt(1);
 
-        // console.log(player1.active);
-        if (player1.active == true) {
-            player1.mark(row, col, 'X');
-            player1.active = false;
-        } else {
-            player2.mark(row, col, 'O');
-            player1.active = true;
+        // Single player scenario
+        if (player2.get_name() == 'Computer') {
+            if (player1.active == true) {
+                player1.mark(row, col, 'X');
+                // player1.active = false;
+            
+                // Random computer selection
+                let i = Math.floor(Math.random() * 3);
+                let j = Math.floor(Math.random() * 3);
+
+                let count = 0;
+                while (gameboard.check_empty_space(i, j) == false) {
+                    i = Math.floor(Math.random() * 3);
+                    j = Math.floor(Math.random() * 3); 
+                    
+                    if (count >= 10) {
+                        return;
+                    }
+                    count++;
+                }
+
+                player2.mark(i, j, 'O');
+                player1.active = true;                
+            }
+            
+        } 
+
+        // Two player scenario
+        if (player2.get_name() != 'Computer') {
+            if (player1.active == true) {
+                player1.mark(row, col, 'X');
+                player1.active = false;
+            } else {
+                player2.mark(row, col, 'O');
+                player1.active = true;
+            }
         }
     });
 }
-
-// Player and Computer Setup
-single_player.addEventListener('click', (e) => {
-    if (single_player.checked == true) {
-        // comp_container.classList.add('inactive');
-        // comp_enable.checked = false;
-    } else {
-        // comp_container.classList.remove('inactive');
-    }
-});
-
-// Start button 
-start_button.addEventListener('click', () => {
-    if (start_button.innerHTML == 'Start') {
-        start_button.innerHTML = 'Reset';
-        gameboard.activate();
-    } else {
-        start_button.innerHTML = 'Reset';
-        gameboard.reset();
-        gameboard.activate();
-    }    
-})
-
 
 const gameboard = (() => {
     var _active = false;
@@ -76,6 +138,10 @@ const gameboard = (() => {
             }
         } 
         win_line.classList.remove('active');
+        message.innerHTML = "";
+
+        player1.active = true;
+        player2.active = false;
         _active = false;      
     }
 
@@ -87,19 +153,28 @@ const gameboard = (() => {
     // Console log gameboard
     const print = () => {console.log(_board);}
 
-    // Display gameboard to DOM
-    const display = () => {
-        for(let i = 0; i < 3; i++) {
-            for(let j = 0; j < 3; j++) {
-                // let element = document.getElementById(`${i}${j}`);
-                // while(element.firstChild) element.removeChild(element.firstChild);
+    const check_empty_space = (row, col) => {
+        if (_board[row][col] == '') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function _empty_space_exists() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (_board[i][j] == '') {
+                    return true;
+                } 
             }
         }
+        return false;
     }
 
     // Mark on gameboard
     const mark = (row, col, marker) => {
-        if (!_active) {return;}   
+        if (!_active) {return false;}   
 
         // Modify gameboard array
         _board[row][col] = marker;
@@ -120,7 +195,19 @@ const gameboard = (() => {
             parent.appendChild(child1);
         }
 
-        let temp = check(row, col, marker);
+        // Check if winner
+        if (check(row, col, marker)) {
+            return true;
+        }
+        
+        // Check if tie 
+        if (_empty_space_exists() == false) {
+            message.innerHTML = "Tie!";
+            _active = false;
+            return false;
+        }
+
+        return false;
     }
 
     // Check horizontal
@@ -195,67 +282,64 @@ const gameboard = (() => {
         const line = document.getElementById('line');
 
         if (check_horizontal(row, marker)) {
-            console.log(`${marker} wins! Horizontal win`);
             line.classList.add('active');
 
             if (row == 0) {line.classList.add('row-top');}
             if (row == 2) {line.classList.add('row-bot');}
-            
             _active = false;
+            return true;
         }
 
         if (check_vertical(col, marker)) {
-            console.log(`${marker} wins! Vertical win`);
             line.classList.add('active');
-
             if (col == 0) {line.classList.add('col-left');}
             if (col == 1) {line.classList.add('col-mid')}
             if (col == 2) {line.classList.add('col-right');}
-
             _active = false;
+            return true;
         }
     
         if (check_pos_diagonal(row, col, marker)) {
-            console.log(`${marker} wins! Positive diagonal win`);
             line.classList.add('active', 'diag-pos');
             _active = false;
+            return true;
         }
 
         if (check_neg_diagonal(row, col, marker)) {
-            console.log(`${marker} wins! Negative diagonal win`);
             line.classList.add('active', 'diag-neg');
             _active = false;
+            return true;
         }
+
+        return false;
     }
 
-    return {reset, activate, print, mark, check};
+    return {reset, activate, print, mark, check, check_empty_space};
 
 })();
 
+
+// Factory Function
 const Player = (name) => {
 
     var active = false;
+    const _name = name; 
 
     const mark = (row, col, marker) => {
-        gameboard.mark(row, col, marker);
+        let temp = gameboard.mark(row, col, marker);
+        if (temp == true) {
+            message.innerHTML = `${name} wins!`;
+        } 
     }
 
     const get_name = () => {
-        console.log(name);
+        return _name;
     }
 
     return {mark, get_name, active};
 }
 
-const player1 = Player('Gavin');
-player1.active = true;
-const player2 = Player('Gabriel');
+var player1 = Player('Player 1');
+player1.active = true; 
+var player2 = Player('Player 2');
 
-
-//Todo
-/* 
-- Ability to select one player or 2 players (toggle)
-- Coin to decide who goes first
-- display to show whose turn it is
-- AI for computer
-*/
